4d0555ed26326e6e3110e1b03bc0e8151b8aa6b3
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-09-05 21:01:35 +0100

    Update main.tsx to enhance app loading

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,22 @@
-
-import { StrictMode } from 'react';
-import './index.css'
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/RoutesPage.tsx';
+import { StrictMode } from "react";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/RoutesPage.tsx";
 import { HelmetProvider } from "react-helmet-async";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { UserContextProvider } from "./context/userContext.tsx";
+import { Toaster } from "react-hot-toast";
+import { AnimatePresence } from "framer-motion";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-   <HelmetProvider>
-      <RouterProvider router={router} />
-   </HelmetProvider>
+    <AnimatePresence>
+      <HelmetProvider>
+        <UserContextProvider>
+          <Toaster />
+          <RouterProvider router={router} />
+        </UserContextProvider>
+      </HelmetProvider>
+    </AnimatePresence>
   </StrictMode>
 );
