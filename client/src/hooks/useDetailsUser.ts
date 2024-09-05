f84e6b0c4af4fd5054db38123892800c55827cf6
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-09-05 21:01:07 +0100

    Improve useDetailsUser for better user details management

--- client/src/hooks/useDetailsUser.ts
+++ client/src/hooks/useDetailsUser.ts
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import axios from 'axios';
 import { logout } from './useAuth';
 import { useUserContext } from '../context/userContext';
+import toast from 'react-hot-toast';
+import { ApiError } from '../types/types';
 
 
 export const useGetUser = () => {
@@ -12,21 +14,26 @@ export const useGetUser = () => {
   const getuser = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(`${import.meta.env.VITE_API_URL}/user-details`, {
-        withCredentials: true,
-      });
-      if (res?.data?.success) {
-      const user=res?.data?.data;
-      setUser({...user,id:user._id,name:user.name})
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/user-details`, { withCredentials: true});
       
-       
+      if (res?.data?.success) {
+       const userdata=res?.data?.user;
+      setUser((prevUser) => ({
+        ...prevUser, 
+        id:userdata._id,name:userdata.username
+      }));
       }
       if (res?.data?.logout) {
         logout();
         return null;
       }
-    } catch (err) {
-      console.log(err);
+    } catch (err: unknown) {
+      const ApiError = err as ApiError;
+      if (ApiError.response?.data.message) {
+        toast.error(ApiError.response.data.message);
+    } else {
+        toast.error("An unexpected error occurred. Please try again later.");
+    }
     } finally {
       setLoading(false);
     }
